ae9895c7ec045b849089f44b3712c362d49768cb
Kittiphan <kittiphan@MacBook-Air-khxng-Kittiphan.local>
2026-01-07 12:28:33 +0700

    Update Next.js to fix security vulnerability

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    unoptimized: true, // เพิ่มบรรทัดนี้
+  },
 };
 
 export default nextConfig;
